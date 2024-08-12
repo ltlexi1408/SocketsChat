ed9a4ab90c3da4e7a517081f7f894404379edbab
ltlexi140 <ltlexi1408@proton.me>
2024-08-12 22:20:50 +1000

    created socket

--- chat.cpp
+++ chat.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <sys/socket.h>
+
+#define PORT 8000
 
 int main(){
-    
+    // Create socket
+    int server = socket(AF_INET, SOCK_STREAM, 0);
+
+    //Check socket status
+    if(server == -1){
+        return 1;
+    }
+
+    return 0;
 }
