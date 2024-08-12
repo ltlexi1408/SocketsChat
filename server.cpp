2f7faee52956d13e2259b61ac53a1037f0b9c1ba
ltlexi140 <ltlexi1408@proton.me>
2024-08-12 22:35:45 +1000

    create/bind/listen on port 8000

--- server.cpp
+++ server.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 
 #define PORT 8000
 
 int main(){
+    //creat socket
+    int server, new_socket;
+
+    if((server = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+        return -1;
+    }
+
+    // create address
+    sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
+    address.sin_addr.s_addr = INADDR_ANY;
+
+    if(bind(server, (struct sockaddr*)&address, sizeof(address)) < 0){
+        return -2;
+    }
+
+    if(listen(server, 5) < 0){
+        return -3;
+    }
 
     return 0;
 }
