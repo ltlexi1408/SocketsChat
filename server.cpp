70db45c5ee856e74787d89b6d5133aa579597ada
ltlexi140 <ltlexi1408@proton.me>
2024-08-12 22:46:23 +1000

    added loop for sending

--- server.cpp
+++ server.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <string>
+#include <cstring>
 
 #define PORT 8000
 
 int main(){
     //creat socket
     int server, new_socket;
+    char *message;
 
     if((server = socket(AF_INET, SOCK_STREAM, 0)) == 0){
         return -1;
@@ -18,13 +22,29 @@ int main(){
     address.sin_port = htons(PORT);
     address.sin_addr.s_addr = INADDR_ANY;
 
+    //binds socket
     if(bind(server, (struct sockaddr*)&address, sizeof(address)) < 0){
         return -2;
     }
 
+    // starts listening for traffic
     if(listen(server, 5) < 0){
         return -3;
     }
+
+    //
+    if(new_socket = accept(server, nullptr, nullptr)< 0){
+        return -4;
+    }
+
+    while(strcmp(message, "q") != 0){
+        std::cin >> message;
+        send(new_socket, message, strlen(message), 0);
+    }
+
+
+    close(new_socket);
+    close(server);
 
     return 0;
 }
